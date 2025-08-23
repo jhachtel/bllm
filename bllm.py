5b7bfc7cb287db2c2779ac6b28ad46c404ee82f5
jhachtel <jeff@hachtel.us>
2025-08-22 22:05:17 -0400

    SimpleTokenizerV2

--- bllm.py
+++ bllm.py
@@ -19,7 +19,7 @@ preprocessed = [item.strip() for item in preprocessed if item.strip()]
 all_words = sorted(set(preprocessed))
 vocab_size = len(all_words)
 
-vocab = {token:integer for integer,token in enumerate(all_words)}
+# vocab = {token:integer for integer,token in enumerate(all_words)}
 
 # for i, item in enumerate(vocab.items()):
 #     print(item)
@@ -27,7 +27,7 @@ vocab = {token:integer for integer,token in enumerate(all_words)}
 #         break
 
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
@@ -35,18 +35,19 @@ class SimpleTokenizerV1:
     def encode(self, text):
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 
-tokenizer = SimpleTokenizerV1(vocab)
-text = "It's the last he painted , you know, Mrs. Gisburn said with pardonable pride."
-ids = tokenizer.encode(text)
+# tokenizer = SimpleTokenizerV2(vocab)
+# text = "It's the last he painted , you know, Mrs. Gisburn said with pardonable pride."
+# ids = tokenizer.encode(text)
 # print(ids)
 
 # print(tokenizer.decode(ids))
@@ -55,10 +56,18 @@ ids = tokenizer.encode(text)
 # print(tokenizer.encode(text))
 
 all_tokens = sorted(list(set(preprocessed)))
-all_tokens.extend(["<|endoftext|>", "|<unk>|"])
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
 vocab = {token:integer for integer,token in enumerate(all_tokens)}
 
-print(len(vocab.items()))
+# print(len(vocab.items()))
 
-for i, item in enumerate(list(vocab.items())[-5:]):
-    print(item)
+# for i, item in enumerate(list(vocab.items())[-5:]):
+#     print(item)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
