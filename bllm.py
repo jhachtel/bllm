fd60cb3c3feec0aa4fbe62feab069642315c4192
jhachtel <jeff@hachtel.us>
2025-08-22 23:49:05 -0400

    End of CH3 - Input Embeddings

--- bllm.py
+++ bllm.py
@@ -49,10 +49,29 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
-data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+# Token Embedding Layer
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
-second_batch = next(data_iter)
-print(second_batch)
+max_length = 4
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+
+token_embeddings = token_embedding_layer(inputs)
+print("\nToken Embeddings shape:\n", token_embeddings.shape)
+
+# Positional Embedding Layer
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print("\nPositional Embeddings shape:\n", pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print("\nInput Embeddings shape:\n", input_embeddings.shape)
+
+
